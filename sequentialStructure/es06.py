0a374fea5409d2e9033134fadc3e5f0b5d838c9d
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 10:46:39 -0300

    Update and rename es06.py to sequentialStructure/es06.py

--- sequentialStructure/es06.py
+++ sequentialStructure/es06.py
@@ -1,4 +1,6 @@
 # Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+# Make a Program that asks for the radius of a circle, calculate and show its area.
+
 import math
 
 radius = float(input('\nEnter the radius of the circle: '))
