17fc11340cf3516f2f1a77cd3de34e0809d9965c
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 10:52:41 -0300

    Update and rename es13.py to sequentialStructure/es13.py

--- sequentialStructure/es13.py
+++ sequentialStructure/es13.py
@@ -2,6 +2,10 @@
 # a) Para homens: (72.7*h) - 58
 # b) Para mulheres: (62.1*h) - 44.7
 
+# Taking as input the height (h) of a person, build an algorithm that calculates your ideal weight, using the following formulas:
+# a) For men: (72.7 * h) - 58
+# b) For women: (62.1 * h) - 44.7
+
 gender = input("\nWhat's your gender (male or female)? ")
 if gender == "male":
     personHeight = float(input("\nEnter your height: "))
