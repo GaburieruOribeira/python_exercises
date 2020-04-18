c14a59bc51cb9da58dac69336566241d08c06b65
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 10:53:28 -0300

    Update and rename es14.py to sequentialStructure/es14.py

--- sequentialStructure/es14.py
+++ sequentialStructure/es14.py
@@ -4,6 +4,12 @@
 # Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da multa que João deverá pagar.
 # Imprima os dados do programa com as mensagens adequadas.
 
+# João Papo-de-Pescador, a good man, bought a microcomputer to control the daily income of his work.
+# Every time he brings a weight of fish greater than that established by the fishing regulation of the state of São Paulo (50 kilos) he must pay a fine of R $ 4.00 per kilo in excess.
+# João needs you to make a program that reads the variable weight (fish weight) and calculates the excess.
+# Record in the excess variable the amount of kilos beyond the limit and in the fine variable the amount of the fine that João must pay.
+# Print the program data with the appropriate messages.
+
 fishWeight = float(input("\nEnter the weight of the fish: "))
 excess = fishWeight - 50.0
 fine = excess * 4.00
