7e61941c17458ccf23ac94cceb0e2414a4de3730
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 16:47:03 -0300

    Update and rename ed12.py to decisionStructure/ed12.py

--- decisionStructure/ed12.py
+++ decisionStructure/ed12.py
@@ -7,6 +7,15 @@
 # - Salário Bruto até 2500 (inclusive) - desconto de 10%
 # - Salário Bruto acima de 2500 - desconto de 20% Imprima na tela as informações, dispostas conforme o exemplo abaixo. No exemplo o valor da hora é 5 e a quantidade de hora é 220.
 
+# Make a program for the calculation of a payroll, knowing that the discounts are from the Income Tax, which depends on the gross salary (as shown in the table below) and 3% for the Union and that the FGTS corresponds to 11% of the Gross Salary , but it is not discounted (it is the company that deposits).
+# Net Salary corresponds to Gross Salary less discounts.
+# The program should ask the user for the value of their hour and the number of hours worked in the month.
+# - IR Discount:
+# - Gross Salary up to 900 (inclusive) - exempt
+# - Gross Salary up to 1500 (inclusive) - 5% discount
+# - Gross Salary up to 2500 (inclusive) - 10% discount
+# - Gross Salary above 2500 - 20% discount Print the information on the screen, arranged according to the example below. In the example the hour value is 5 and the hour amount is 220.
+
 hourlyWage = float(input('\nHow much salary do you earn per hour? R$'))
 workedHours = float(input('\nHow many hours do you work in this month? '))
 grossSalary = hourlyWage * workedHours
