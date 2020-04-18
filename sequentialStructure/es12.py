c7245b1242af6650e6c4973598b1ce05a4d1718e
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 10:51:55 -0300

    Update and rename es12.py to sequentialStructure/es12.py

--- sequentialStructure/es12.py
+++ sequentialStructure/es12.py
@@ -1,6 +1,9 @@
 # Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula:
 # (72.7*altura) - 58
 
+# Using a person's height as input data, build an algorithm that calculates your ideal weight, using the following formula:
+# (72.7 * height) - 58
+
 personHeight = float(input("\nEnter your height: "))
 idealWeight = ( 72.7 * personHeight ) - 58
 print("\nYour ideal weight is: {:.2f}Kg".format(idealWeight))
