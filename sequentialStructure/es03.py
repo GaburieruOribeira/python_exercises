9e5e77e81557ecdd77671b764d16ea7088add4d7
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 10:42:36 -0300

    Update es03.py

--- sequentialStructure/es03.py
+++ sequentialStructure/es03.py
@@ -2,9 +2,9 @@
 # Make a program that asks for two numbers and prints the sum.
 
 try:
-    num1 = int(input('\nDigite um número: '))
-    num2 = int(input('Digite outro número: '))
+    num1 = int(input('\nEnter a number: '))
+    num2 = int(input('Enter another number: '))
     sum = num1 + num2
     print('\n{} + {} = {}'.format(num1, num2, sum))
 except ValueError:
-    print('\nVocê não digitou um número')
+    print('\nYou did not enter a number.')
