1dfc333bf556152e750867c294776bb199c4f975
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 10:49:00 -0300

    Update es09.py

--- es09.py
+++ es09.py
@@ -1,4 +1,5 @@
-# Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius.
+# Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+# Make a Program that asks for the temperature in degrees Fahrenheit, transform and show the temperature in degrees Celsius.
 
 fahrenheit = float(input('\nEnter degrees in Fahrenheit: '))
 celsius = (5 * (fahrenheit - 32) / 9)
