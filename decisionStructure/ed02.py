5dbf49131008961ddce681d374a615966e72f5a2
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 16:36:53 -0300

    Update and rename ed02.py to decisionStructure/ed02.py

--- decisionStructure/ed02.py
+++ decisionStructure/ed02.py
@@ -1,5 +1,5 @@
 # Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
-
+# Make a Program that asks for a value and shows on the screen whether the value is positive or negative.
 
 number = int(input("\nEnter a number: "))
 if number >= 0:
