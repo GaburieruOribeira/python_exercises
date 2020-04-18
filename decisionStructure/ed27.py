1f218bb6a4d964e7571950d3b6acc35462cded5c
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 16:59:45 -0300

    Update and rename ed27.py to decisionStructure/ed27.py

--- decisionStructure/ed27.py
+++ decisionStructure/ed27.py
@@ -5,6 +5,13 @@
 # Se o cliente comprar mais de 8 Kg em frutas ou o valor total da compra ultrapassar R$ 25,00, receberá ainda um desconto de 10% sobre este total.
 # Escreva um algoritmo para ler a quantidade (em Kg) de morangos e a quantidade (em Kg) de maças adquiridas e escreva o valor a ser pago pelo cliente.
 
+# A fruit tree is selling fruit with the following price list:
+#                     Up to 5 Kg              Above 5 Kg
+# Strawberry       R$2.50 per Kg           R$2.20 per Kg
+# Apple            R$1.80 per kg           R$1.50 per kg
+# If the customer buys more than 8 kg of fruit or the total purchase value exceeds R$25.00, he will also receive a 10% discount on this total.
+# Write an algorithm to read the quantity (in Kg) of strawberries and the quantity (in Kg) of apples purchased and write the amount to be paid by the customer.
+
 print("""\nPrice Table:
                     Up to 5 Kg               Over 5 Kg
 Strawberry      R$ 2,50 per Kg          R$ 2,20 per Kg
@@ -29,4 +36,4 @@ totalValue = strawberryPrice + applePrice
 if totalAmount > 8.0 or totalValue > 25.0:
     totalValue = totalValue - ((totalValue * 10) / 100)
 
-print("\nQuantidade total (Kg): {}Kg\nPreço Final: R${:.2f}".format(totalAmount, totalValue))
+print("\nTotal amount (Kg): {}Kg\nFinal price: R${:.2f}".format(totalAmount, totalValue))
