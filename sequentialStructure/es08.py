1d58401e5fe8167057a66b860863223ab8544874
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 10:48:19 -0300

    Update and rename es08.py to sequentialStructure/es08.py

--- sequentialStructure/es08.py
+++ sequentialStructure/es08.py
@@ -1,4 +1,5 @@
 # Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+# Make a Program that asks how much you earn per hour and the number of hours worked in the month. Calculate and show your total salary for that month.
 
 hourlyWage = float(input('\nHow much salary do you earn per hour? US$'))
 workedHours = float(input('\nHow many hours do you work in this month? '))
