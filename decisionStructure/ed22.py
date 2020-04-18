ac7ae09bc46b844a87af49f39f99398fa42c6e75
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 16:53:56 -0300

    Update and rename ed22.py to decisionStructure/ed22.py

--- decisionStructure/ed22.py
+++ decisionStructure/ed22.py
@@ -1,4 +1,5 @@
 # Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).
+# Make a Program that asks for a whole number and determine if it is even or odd. Tip: use the module operator (rest of the division).
 
 number = int(input("\nEnter a number: "))
 if number % 2 == 0:
