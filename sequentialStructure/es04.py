8b65cc8406511fb25fc6c47cacfa37d111951e6f
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 10:44:35 -0300

    Update es04.py

--- sequentialStructure/es04.py
+++ sequentialStructure/es04.py
@@ -1,16 +1,16 @@
 # Faça um Programa que peça as 4 notas bimestrais e mostre a média.
-# # Make a Program that asks for the 4 bimonthly notes and shows the average.
+# Make a Program that asks for the 4 bimonthly notes and shows the average.
 
 medias = []
-medias.append(input('\nDigite a primeira média: '))
-medias.append(input('Digite a segunda média: '))
-medias.append(input('Digite a terceira média: '))
-medias.append(input('Digite a quarta média: '))
+medias.append(input('\nEnter the first average: '))
+medias.append(input('Enter the second average: '))
+medias.append(input('Enter the third average: '))
+medias.append(input('Enter the fourth average: '))
 medias_float = []
 try:
     for item in medias:
         medias_float.append(float(item))
     sum = (medias_float[0] + medias_float[1] + medias_float[2] + medias_float[3]) / len(medias)
-    print('\nA média final é: {}'.format(sum))
+    print('\nThe final average is: {}'.format(sum))
 except ValueError:
-    print('\nVocê não digitou um número')
+    print('\nYou did not enter a number.')
