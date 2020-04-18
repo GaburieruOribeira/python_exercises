d1cd993193cb31302e482f35faa25b03604a4ae0
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 16:37:49 -0300

    Update and rename ed03.py to decisionStructure/ed03.py

--- decisionStructure/ed03.py
+++ decisionStructure/ed03.py
@@ -1,4 +1,5 @@
 # Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+# Make a program that checks if a typed letter is "F" or "M". As the letter writes: F - Female, M - Male, Invalid Gender.
 
 gender = str(input("""What's your gender:
 F - Feminine
