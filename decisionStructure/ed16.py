033dfd392f9a9e88afe94ef19b5b4ba10d09daa6
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 16:49:25 -0300

    Update and rename ed16.py to decisionStructure/ed16.py

--- decisionStructure/ed16.py
+++ decisionStructure/ed16.py
@@ -5,6 +5,13 @@
 # - Se o delta calculado for igual a zero a equação possui apenas uma raiz real; informe-a ao usuário;
 # - Se o delta for positivo, a equação possui duas raiz reais; informe-as ao usuário;
 
+# Make a program that calculates the roots of a second degree equation, in the form ax2 + bx + c.
+# The program should ask for the values of a, b and c and make the consistencies, informing the user in the following situations:
+# - If the user inputs the value of A equal to zero, the equation is not of the second degree and the program should not ask for the other values, being closed;
+# - If the calculated delta is negative, the equation has no real roots. Inform the user and close the program;
+# - If the calculated delta is equal to zero, the equation has only one real root; inform the user;
+# - If the delta is positive, the equation has two real roots; inform the user;
+
 import math
 
 a = float(input("\nEnter the A value: "))
