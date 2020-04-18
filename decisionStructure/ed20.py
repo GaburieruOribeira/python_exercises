02dc8e0732282451d0f0d912feb2a145dad7bb6a
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 16:51:58 -0300

    Update and rename ed20.py to decisionStructure/ed20.py

--- decisionStructure/ed20.py
+++ decisionStructure/ed20.py
@@ -3,6 +3,11 @@
 # - A mensagem "Reprovado", se a média for menor do que 7, com a respectiva média alcançada;
 # - A mensagem "Aprovado com Distinção", se a média for igual a 10.
 
+# Make a Program to read a student's three partial grades. The program should calculate the average achieved per student and present:
+# - The message "Approved", if the average is greater than or equal to 7, with the respective average achieved;
+# - The message "Failed", if the average is less than 7, with the respective average achieved;
+# - The message "Approved with Distinction", if the average is equal to 10.
+
 note1 = float(input("\nEnter the first note: "))
 note2 = float(input("\nEnter the second note: "))
 note3 = float(input("\nEnter the third note: "))
