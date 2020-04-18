483a4030d57ce13021b72fc9ee63d4311671ff78
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 10:47:34 -0300

    Update and rename es07.py to sequentialStructure/es07.py

--- sequentialStructure/es07.py
+++ sequentialStructure/es07.py
@@ -1,4 +1,5 @@
 # Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+# Make a program that calculates the area of a square, then show twice the area to the user.
 
 squareSide = float(input('\nEnter the size of the one side of the square: '))
 squareArea = squareSide * squareSide
