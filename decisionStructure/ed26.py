5a5d9cc54138493a824fb17c87c63f93ae55bebd
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 16:57:41 -0300

    Update and rename ed26.py to decisionStructure/ed26.py

--- decisionStructure/ed26.py
+++ decisionStructure/ed26.py
@@ -8,6 +8,16 @@
 # Escreva um algoritmo que leia o número de litros vendidos, o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina),
 # calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 2,50 o preço do litro do álcool é R$ 1,90.
 
+# A gas station is selling fuel with the following discount table:
+# - Alcohol:
+# - up to 20 liters, 3% discount per liter;
+# - above 20 liters, discount of 5% per liter;
+# - Gasoline:
+# - up to 20 liters, discount of 4% per liter;
+# - above 20 liters, discount of 6% per liter;
+# Write an algorithm that reads the number of liters sold, the type of fuel (coded as follows: A-alcohol, G-gasoline),
+# calculate and print the amount to be paid by the customer knowing that the price of a liter of gasoline is R $ 2.50 the price of a liter of alcohol is R $ 1.90.
+
 print("""\nDiscount Table:
 + Alcohol:
     - Up to 20 liters, 3% discount per liter;
