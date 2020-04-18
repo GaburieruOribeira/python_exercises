b95394e6c810d2049bef80f7ec89258a8f0d129c
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 16:51:25 -0300

    Update and rename ed19.py to decisionStructure/ed19.py

--- decisionStructure/ed19.py
+++ decisionStructure/ed19.py
@@ -2,6 +2,12 @@
 # Observando os termos no plural a colocação do "e", da vírgula entre outros. Exemplo:
 # - 326 = 3 centenas, 2 dezenas e 6 unidades
 # - 12 = 1 dezena e 2 unidades Testar com: 326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20, 10, 21, 11, 1, 7 e 16
+
+# Make a Program that reads an integer less than 1000 and prints the number of hundreds, tens and units of it.
+# Observing the plural terms the placement of the "e", the comma among others. Example:
+# - 326 = 3 hundreds, 2 tens and 6 units
+# - 12 = 1 dozen and 2 units Test with: 326, 300, 100, 320, 310,305, 301, 101, 311, 111, 25, 20, 10, 21, 11, 1, 7 and 16
+
 import math
 
 number = int(input("\nEnter a number: "))
