47a0d1b46df09934fa162b27b7aac3eb55e5f686
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 10:41:31 -0300

    Update es02.py

--- sequentialStructure/es02.py
+++ sequentialStructure/es02.py
@@ -1,10 +1,10 @@
 # Faça um Programa que peça um número e então mostre a mensagem: O número informado foi [número].
 # Make a Program that asks for a number and then displays the message: The number entered was [number].
 
-number = input('\nInforme um número: ')
+number = input('\nEnter a number: ')
 try:
     number1 = int(number)
     if number1 == int(number):
-        print('\nO número informado foi: {}'.format(number))
+        print('\nThe number entered was: {}'.format(number))
 except ValueError:
-    print('\nVocê não digitou um número.')
+    print('\nYou did not enter a number.')
