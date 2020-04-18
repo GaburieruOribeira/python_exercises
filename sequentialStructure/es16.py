fc4596563125e38f4bd1409a23f7fd4f78cba58f
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 10:55:12 -0300

    Update and rename es16.py to sequentialStructure/es16.py

--- sequentialStructure/es16.py
+++ sequentialStructure/es16.py
@@ -3,6 +3,11 @@
 # Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00.
 # Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
 
+# Make a program for a paint store.
+# The program must request the size in square meters of the area to be painted.
+# Consider that the paint coverage is 1 liter for every 3 square meters and that the paint is sold in 18-liter cans, which cost R $ 80.00.
+# Inform the user of the quantity of paint cans to be purchased and the total price.
+
 areaToBePainted = float(input("\nEnter the size of the area to be painted(m²): "))
 inkCover = ( 1 / 3 ) * areaToBePainted
 paintCans = inkCover / 18
