d5e8cb9f4d11f06fcee1da69703515d2d777a541
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 16:54:35 -0300

    Update and rename ed23.py to decisionStructure/ed23.py

--- decisionStructure/ed23.py
+++ decisionStructure/ed23.py
@@ -1,4 +1,5 @@
 # Faça um Programa que peça um número e informe se o número é inteiro ou decimal. Dica: utilize uma função de arredondamento.
+# Make a program that asks for a number and inform if the number is an integer or a decimal. Tip: use a rounding function.
 
 number = float(input("\nEnter a number: "))
 
