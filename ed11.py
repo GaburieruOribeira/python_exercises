cb3a6ba83eced40883f25c1041bc8f5f23ae40fd
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 16:44:01 -0300

    Update ed11.py

--- ed11.py
+++ ed11.py
@@ -1,4 +1,4 @@
-# As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contraram para desenvolver o programa que calculará os reajustes.
+# As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contrataram para desenvolver o programa que calculará os reajustes.
 #Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual:
 # - Salários até R$ 280,00 (incluindo) : aumento de 20%
 # - Salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
@@ -8,6 +8,19 @@
 # - O percentual de aumento aplicado;
 # - O valor do aumento;
 # - O novo salário, após o aumento.
+
+
+689/5000
+# Tabajara Organizations decided to give their employees a salary increase and hired them to develop the program that will calculate the adjustments.
+# Make a program that receives an employee's salary and readjust it according to the following criteria, based on the current salary:
+# - Salaries up to R $ 280.00 (including): 20% increase
+# - Salaries between R $ 280.00 and R $ 700.00: 15% increase
+# - Salaries between R $ 700.00 and R $ 1500.00: 10% increase
+# - Salaries from R $ 1500.00 onwards: 5% increase After the increase is made, inform on the screen:
+# - The salary before the adjustment;
+# - The percentage of increase applied;
+# - The amount of the increase;
+# - The new salary, after the increase.
 
 salary = float(input("\nEnter the salary: R$"))
 
