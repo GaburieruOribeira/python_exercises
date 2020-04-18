55a5b1b4bd329e481f338b65c6325fafacc13426
Gabriel Oliveira <62845480+GaburieruOribeira@users.noreply.github.com>
2020-04-18 10:56:49 -0300

    Update and rename es18.py to sequentialStructure/es18.py

--- sequentialStructure/es18.py
+++ sequentialStructure/es18.py
@@ -1,6 +1,9 @@
 # Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps),
 # calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
 
+# Make a program that asks for the size of a file to download (in MB) and the speed of an Internet link (in Mbps),
+# calculate and inform the approximate time to download the file using this link (in minutes).
+
 fileSize = input("\nHow big is the file to be downloaded(Mb)? ")
 internetSpeed = input("\nWhat is the speed of your internet(Mbps)? ")
 downloadTime = (float(fileSize) / float(internetSpeed)) * 60
